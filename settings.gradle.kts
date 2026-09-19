cef90ebdf6f09b666a85fd348c6217df4ecf0a1b
Meteor2333 <Meteor23333@outlook.com>
2026-09-19 21:09:37 +0800

    refactor: 移除jadx插件模块

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,12 +25,10 @@ include(
     ":xposedkit",
     ":xposedkit-gradle-plugin",
     ":xposedkit-hidden-api",
-    ":xposedkit-jadx-plugin",
     ":xposedkit-native",
     ":xposedkit-processor"
 )
 project(":xposedkit-gradle-plugin").projectDir = file("gradle-plugin")
 project(":xposedkit-hidden-api").projectDir = file("hidden-api")
-project(":xposedkit-jadx-plugin").projectDir = file("jadx-plugin")
 project(":xposedkit-native").projectDir = file("native")
 project(":xposedkit-processor").projectDir = file("processor")
